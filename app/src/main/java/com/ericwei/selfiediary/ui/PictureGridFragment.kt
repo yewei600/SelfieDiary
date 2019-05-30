25918e36f703e301ede9411e9f785a35689a272c
Eric Wei <ericwei94@gmail.com>
2019-05-30 14:39:58 -0400

    launch the camera app

--- app/src/main/java/com/ericwei/selfiediary/ui/PictureGridFragment.kt
+++ app/src/main/java/com/ericwei/selfiediary/ui/PictureGridFragment.kt
@@ -1,6 +1,8 @@
 package com.ericwei.selfiediary.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,18 +16,22 @@ import com.ericwei.selfiediary.viewmodels.PictureGridViewModel
 
 class PictureGridFragment : Fragment() {
 
+    val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding: FragmentPictureGridBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_picture_grid, container, false
         )
-        val mViewModel = ViewModelProviders.of(
-            this, InjectorUtils.providePictureGridViewModelFactory(
-                requireContext()
-            )
-        )
-            .get(PictureGridViewModel::class.java)
-        val adapter = PictureGridAdapter()
+
+        val viewModel = ViewModelProviders.of(
+            this, InjectorUtils.providePictureGridViewModelFactory(requireContext())
+        ).get(PictureGridViewModel::class.java)
+
+        //binding.photosGrid.adapter = PictureGridAdapter()
+
+        binding.pictureBtn.setOnClickListener {
+            onPictureButtonClicked()
+        }
 
         return binding.root
     }
@@ -36,4 +42,16 @@ class PictureGridFragment : Fragment() {
 //        })
     }
 
+    //launch intent to take picture
+    fun onPictureButtonClicked() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(context!!.packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 }
